93172d094df98d55d66973bb5472ec7833ebfd8a
Irfan <irfan.ahmed6611@gmail.com>
2024-12-11 23:37:18 +0400

    use  'compactMapValues' method instead of 'reduce' to remove nil values from a Dictionary.

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -108,9 +108,8 @@ class RemoteFeedLoaderTests: XCTestCase {
             "description": item.description,
             "location": item.location,
             "image": item.imageURL.absoluteString
-        ].reduce(into: [String: Any]()) { (acc, e) in
-            if let value = e.value {acc[e.key] = value}
-        }
+        ].compactMapValues { $0 }
+
         
        return (item, json)
         
