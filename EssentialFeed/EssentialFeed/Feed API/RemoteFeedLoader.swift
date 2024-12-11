366282c62cee5546f677d13459d0879b5a995137
Irfan <irfan.ahmed6611@gmail.com>
2024-12-11 16:53:55 +0400

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -40,8 +40,8 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, _):
-                if let _ = try? JSONSerialization.jsonObject(with: data) {
-                    complition(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    complition(.success(root.items))
                 } else {
                     complition(.failiur(.invalidDate))
                 }
@@ -50,5 +50,8 @@ public final class RemoteFeedLoader {
             }
         }
     }
-    
+}
+
+private struct Root: Decodable {
+    let items: [FeedItem]
 }
