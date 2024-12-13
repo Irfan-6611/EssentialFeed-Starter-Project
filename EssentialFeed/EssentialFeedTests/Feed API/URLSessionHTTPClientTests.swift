2f4cec1b6fbb5a5c0bf70581617e4ee4d41fec15
Irfan <irfan.ahmed6611@gmail.com>
2024-12-13 11:56:11 +0400

    Add memory leak tracking

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -36,7 +36,6 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_getFromURL_performGETRequestWithURL() {
         let url = URL(string: "https://a-url.com")!
-        let sut = URLSessionHTTPClient()
 
         let exp = expectation(description: "wait for request")
         
@@ -46,7 +45,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        sut.get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -55,11 +54,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
-        let sut = URLSessionHTTPClient()
         
         let exp = expectation(description: "wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failiur(receivedError as NSError):
                 XCTAssertNotNil(receivedError)
@@ -74,6 +72,19 @@ class URLSessionHTTPClientTests: XCTestCase {
         
     }
     
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
+        let sut = URLSessionHTTPClient()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated, Potenial memory leaks", file: file, line: line)
+        }
+    }
 }
 
 // MARK: - Helpers
