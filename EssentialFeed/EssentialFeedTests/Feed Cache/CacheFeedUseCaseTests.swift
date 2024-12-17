9424063027432288e83595120bc287687cef5901
Irfan-6611 <irfan.ahmed6611@gmail.com>
2024-12-17 15:04:58 +0400

    save command requests cache deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,14 +6,25 @@
 //
 
 import XCTest
-
+import EssentialFeed
 
 class FeedStore {
     var deleteCacheFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCacheFeedCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
+    private let store: FeedStore
+
     init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
@@ -24,6 +35,26 @@ final class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCacheFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        sut.save([anyFeedItem(), anyFeedItem()])
+        
+        XCTAssertEqual(store.deleteCacheFeedCallCount, 1)
+        
+    }
+    
+    // MARK: - Helpers
+    
+    private func anyFeedItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "Any Desc", location: "Any Location", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any.url.com")!
     }
     
 }
