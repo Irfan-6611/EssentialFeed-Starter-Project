80b541caf81e30e3b8e044024677b28a32348534
Irfan-6611 <irfan.ahmed6611@gmail.com>
2025-01-12 16:08:24 +0400

    Hide loading indicator on loader completion

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -23,7 +23,7 @@ final class FeedViewController: UITableViewController {
         
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        load()
+        refresh()
     }
     
     override func viewIsAppearing(_ animated: Bool) {
@@ -41,7 +41,9 @@ final class FeedViewController: UITableViewController {
     }
     
     private func load() {
-        loader?.load { _ in }
+        loader?.load { [unowned self] _ in
+            self.refreshControl?.endRefreshing()
+        }
     }
 }
 
@@ -87,6 +89,15 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
     
+    func test_viewDidLoad_hidesLoadingIndicatorOnLoaderCompletion() {
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading()
+        
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+    }
+    
     //MARK: - Helper
     
     private func makeSUT(file: StaticString = #filePath,
@@ -99,10 +110,18 @@ final class FeedViewControllerTests: XCTestCase {
     }
     
     class LoaderSpy: FeedLoader {
-        private(set) var loadCallCount = 0
-        
+        private var completions = [(FeedLoader.Result) -> Void]()
+
+        var loadCallCount: Int {
+            return completions.count
+        }
+            
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
-            loadCallCount += 1
+            completions.append(completion)
+        }
+        
+        func completeFeedLoading(at index: Int = 0) {
+            completions[0](.success([]))
         }
     }
 }
